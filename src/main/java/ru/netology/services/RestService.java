05fc4e5654fb44b584395af31998d07ef95a0b98
Galina <gana88tit@gmail.com>
2023-09-24 09:48:38 +0300

    Initial commit

---
+++ src/main/java/ru/netology/services/RestService.java
@@ -0,0 +1,21 @@
+package ru.netology.services;
+
+public class RestService {
+    public int calculate(int income, int expenses, int threshold) {
+        int money = 0;
+        int counter = 0;
+        for (int i = 0; i < 12; i++) {
+            if (money >= threshold) {
+                //отдыхает
+                money = money - expenses;
+                money = money / 3;
+                counter = counter + 1;
+            } else {
+                //работает
+                money = money + income;
+                money = money - expenses;
+            }
+        }
+        return (counter);
+    }
+}
